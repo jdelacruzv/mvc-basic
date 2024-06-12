328063516db3f4ec1e3b81f36bb0c5986774954a
Jose De La Cruz <jldlcv@gmail.com>
2024-06-12 23:18:58 +0200

    Exceptions file added

--- basic_backend.py
+++ basic_backend.py
@@ -1,5 +1,16 @@
+import mvc_exceptions as mvc_exc
+
 # global variable where we keep the data
 items = list() 
+
+
+def create_item(name, price, quantity):
+	global items
+	results = list(filter(lambda x: x['name'] == name, items))
+	if results:
+		raise mvc_exc.ItemAlreadyStored(f'{name} already stored!')
+	else:
+		items.append({'name': name, 'price': price, 'quantity': quantity})
 
 
 def create_items(app_items):
@@ -7,9 +18,13 @@ def create_items(app_items):
 	items = app_items
 
 
-def create_item(name, price, quantity):
+def read_item(name):
 	global items
-	items.append({'name': name, 'price': price, 'quantity': quantity})
+	my_items = list(filter(lambda x: x['name'] == name, items))
+	if my_items:
+		return my_items[0]
+	else:
+		raise mvc_exc.ItemNotStored(f'Can\'t read {name} because it\'s not stored!')
 
 
 def read_items():
@@ -17,7 +32,3 @@ def read_items():
 	return [item for item in items]
 
 
-def read_item(name):
-	global items
-	my_items = list(filter(lambda x: x['name'] == name, items))
-	return my_items[0]
