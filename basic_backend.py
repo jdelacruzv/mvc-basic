0c2fc1e2dba01c1718e093a6c95331b11f0efec5
Jose De La Cruz <jldlcv@gmail.com>
2024-06-16 10:51:14 +0200

    Update and delete items functions added

--- basic_backend.py
+++ basic_backend.py
@@ -32,3 +32,25 @@ def read_items():
 	return [item for item in items]
 
 
+def update_item(name, price, quantity):
+	global items
+	# i_x is a tuple, idxs_items is a list of tuples
+	idxs_items = list(filter(lambda i_x: i_x[1]['name'] == name, enumerate(items)))
+	if idxs_items:
+		i, item_to_update = idxs_items[0][0], idxs_items[0][1]
+		items[i] = {'name': name, 'price': price, 'quantity': quantity}
+	else:
+		raise mvc_exc.ItemNotStored(f'Can\'t update {name} because it\'s not stored!')
+
+
+def delete_item(name):
+	global items
+	# i_x is a tuple, idxs_items is a list of tuples
+	idxs_items = list(
+		filter(lambda i_x: i_x[1]['name'] == name, enumerate(items)))
+	if idxs_items:
+		i, item_to_delete = idxs_items[0][0], idxs_items[0][1]
+		del items[i]
+	else:
+		raise mvc_exc.ItemNotStored(
+			f'Can\'t delete {name} because it\'s not stored!')
